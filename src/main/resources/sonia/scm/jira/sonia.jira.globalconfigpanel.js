25eba3603e26fd4960b4502e38eddbaaafa25ae6
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2012-10-09 17:09:18 +0200

    added global configuration panel

--- src/main/resources/sonia/scm/jira/sonia.jira.globalconfigpanel.js
+++ src/main/resources/sonia/scm/jira/sonia.jira.globalconfigpanel.js
@@ -1,10 +1,10 @@
-/*
+/* *
  * Copyright (c) 2010, Sebastian Sdorra
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
@@ -13,7 +13,7 @@
  * 3. Neither the name of SCM-Manager; nor the names of its
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -24,80 +24,115 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * http://bitbucket.org/sdorra/scm-manager
- *
+ * 
  */
-Ext.ns('Sonia.jira');
 
-Sonia.jira.RepositoryConfigPanel = Ext.extend(Sonia.repository.PropertiesFormPanel, {
-  
+Ext.ns("Sonia.jira");
+
+Sonia.jira.GlobalConfigPanel = Ext.extend(Sonia.config.ConfigForm, {
+
   initComponent: function(){
+
     var config = {
-      title: Sonia.jira.I18n.formTitleText,
-      items: [{
-        name: 'jiraUrl',
-        fieldLabel: Sonia.jira.I18n.urlText,
-        property: 'jira.url',
+      title : Sonia.jira.I18n.titleText,
+      items : [{
+        xtype : 'textfield',
+        fieldLabel : Sonia.jira.I18n.urlText,
+        name : 'url',
         vtype: 'url',
+        allowBlank : true,
         helpText: Sonia.jira.I18n.urlHelpText
       },{
-        id: 'jiraUpdateIssues',
-        name: 'jiraUpdateIssues',
+        xtype: 'checkbox',
+        fieldLabel : Sonia.jira.I18n.repositoryConfigurationText,
+        name: 'disable-repository-configuration',
+        inputValue: 'true',
+        helpText: Sonia.jira.I18n.repositoryConfigurationHelpText
+      },{
+        name: 'update-issues',
         xtype: 'checkbox',
         fieldLabel: Sonia.jira.I18n.updateIssuesText,
-        property: 'jira.update-issues',
         helpText: Sonia.jira.I18n.updateIssuesHelpText,
         listeners: {
           check: this.toggleUpdateIssues
         }
       },{
-        id: 'jiraAutoClose',
-        name: 'jiraAutoClose',
+        id: 'autoClose',
+        name: 'auto-close',
         xtype: 'checkbox',
         fieldLabel: Sonia.jira.I18n.autoCloseText,
-        property: 'jira.auto-close',
         helpText: Sonia.jira.I18n.autoCloseHelpText
       },{
-        id: 'jiraAutoCloseWords',
-        name: 'jiraAutoCloseWords',
-        fieldLabel: Sonia.jira.I18n.autoCloseWordsText,
-        property: 'jira.auto-close-words',
+        id: 'autoCloseWords',
+        name: 'auto-close-words',
+        fieldLabel: this.autoCloseWordsText,
         helpText: Sonia.jira.I18n.autoCloseWordsHelpText,
         value: Sonia.jira.I18n.autoCloseDefaultValues
       }]
     }
-    
+
     Ext.apply(this, Ext.apply(this.initialConfig, config));
-    Sonia.jira.RepositoryConfigPanel.superclass.initComponent.apply(this, arguments);
-  },
-  
-  loadExtraProperties: function(item){
-    var cmp = Ext.getCmp('jiraUpdateIssues');
-    this.toggleUpdateIssues.call(cmp);
+    Sonia.jira.GlobalConfigPanel.superclass.initComponent.apply(this, arguments);
   },
   
   toggleUpdateIssues: function(){
     var cmps = [
-      Ext.getCmp( 'jiraAutoClose' ),
-      Ext.getCmp( 'jiraAutoCloseWords' )
+      Ext.getCmp( 'autoClose' ),
+      Ext.getCmp( 'autoCloseWords' )
     ];
     
     Sonia.jira.toggleFields(cmps);
+  },
+
+  onSubmit: function(values){
+    this.el.mask(this.submitText);
+    Ext.Ajax.request({
+      url: restUrl + 'plugins/jira/global-config.json',
+      method: 'POST',
+      jsonData: values,
+      scope: this,
+      disableCaching: true,
+      success: function(response){
+        this.el.unmask();
+      },
+      failure: function(){
+        this.el.unmask();
+      }
+    });
+  },
+
+  onLoad: function(el){
+    var tid = setTimeout( function(){
+      el.mask(this.loadingText);
+    }, 100);
+    Ext.Ajax.request({
+      url: restUrl + 'plugins/jira/global-config.json',
+      method: 'GET',
+      scope: this,
+      disableCaching: true,
+      success: function(response){
+        var obj = Ext.decode(response.responseText);
+        this.load(obj);
+        clearTimeout(tid);
+        el.unmask();
+      },
+      failure: function(){
+        el.unmask();
+        clearTimeout(tid);
+        alert('failure');
+      }
+    });
   }
-  
+
 });
 
 // register xtype
-Ext.reg("jiraRepositoryConfigPanel", Sonia.jira.RepositoryConfigPanel);
+Ext.reg("jiraGlobalConfigPanel", Sonia.jira.GlobalConfigPanel);
 
-// register panel
-Sonia.repository.openListeners.push(function(repository, panels){
-  if (Sonia.repository.isOwner(repository)){
-    panels.push({
-      xtype: 'jiraConfigPanel',
-      item: repository
-    });
-  }
+// regist config panel
+registerGeneralConfigPanel({
+  id: 'jiraGlobalConfigPanel',
+  xtype: 'jiraGlobalConfigPanel'
 });
-
---
+++ src/main/resources/sonia/scm/jira/sonia.jira.js
@@ -0,0 +1,76 @@
+/* *
+ * Copyright (c) 2010, Sebastian Sdorra
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of SCM-Manager; nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * http://bitbucket.org/sdorra/scm-manager
+ * 
+ */
+
+Ext.ns("Sonia.jira");
+
+Sonia.jira.I18n = {
+  
+  titleText: 'Jira Configuration',
+  formTitleText: 'Jira',
+  
+  urlText: 'Url',
+  urlHelpText: 'Url of Jira installation (with contextpath).',
+  
+  repositoryConfigurationText: 'Do not allow repository configuration',
+  repositoryConfigurationHelpText: 'Do not allow repository owners to configure jira instances. \n\
+    You have to restart your application server after changing this value.',
+  
+  autoCloseText: 'Enable Auto-Close',
+  autoCloseHelpText: 'Enables the auto close function. SCM-Manager searches for \n\
+                      issue keys and auto close words in commit messages. If \n\
+                      both found in a message SCM-Manager closes the issue in \n\
+                      the jira server. <strong>Note:</strong> It is necessary \n\
+                      that users have the same name and password in SCM-Manager and Jira.',
+  autoCloseDefaultValues: 'fixed, fix, closed, close, resolved, resolve',
+  
+  updateIssuesText: 'Update Jira Issues',
+  updateIssuesHelpText: 'Enable the automatic update function. SCM-Manager searches for\n\
+                         issue keys in commit messages. If a issue id is found SCM-Manager\n\
+                         updates the issue with a comment. <strong>Note:</strong> It \n\
+                         is necessary that users have the same name and password in SCM-Manager \n\
+                         and Jira.',
+  
+  autoCloseWordsText: 'Auto-Close Words',
+  autoCloseWordsHelpText: 'Comma separated list of words to enable the auto close function. \n\
+                           Each commit message of a changeset is being searched for these words.'
+  
+}
+
+Sonia.jira.toggleFields = function(cmps){
+  Ext.each(cmps, function(cmp){
+    cmp.setReadOnly(!this.checked);
+    if ( ! this.checked ){
+      cmp.addClass('x-item-disabled');
+    } else {
+      cmp.removeClass('x-item-disabled');
+    }
+  }, this);
+}
