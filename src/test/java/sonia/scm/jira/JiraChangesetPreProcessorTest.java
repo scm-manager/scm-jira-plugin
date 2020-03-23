6926995ce8c325689148575ec4e8e5163618bd62
Florian Scholdei <45232454+fscholdei@users.noreply.github.com>
2020-03-23 14:05:46 +0100

    Changeover to MIT license (#3)
    * Changeover to MIT license
    * Remove unnecessary stubbing
    
    Co-authored-by: René Pfeuffer <rene.pfeuffer@cloudogu.com>

--- src/test/java/sonia/scm/jira/JiraChangesetPreProcessorTest.java
+++ src/test/java/sonia/scm/jira/JiraChangesetPreProcessorTest.java
@@ -1,33 +1,25 @@
-/**
- * *
- * Copyright (c) 2015, Sebastian Sdorra
- * All rights reserved.
+/*
+ * MIT License
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
  *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of SCM-Manager; nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * https://bitbucket.org/sdorra/scm-manager
- *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package sonia.scm.jira;
 
@@ -37,7 +29,9 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import sonia.scm.repository.Changeset;
 
 /**
  * Unit tests for {@link JiraChangesetPreProcessor}.
@@ -45,7 +39,10 @@ import org.mockito.runners.MockitoJUnitRunner;
  * @author Sebastian Sdorra <sebastian.sdorra@triology.de>
  */
 @RunWith(MockitoJUnitRunner.class)
-public class JiraChangesetPreProcessorTest extends JiraTestBase {
+public class JiraChangesetPreProcessorTest {
+
+  @Mock
+  protected Changeset changeset;
 
   private JiraChangesetPreProcessor processor;
 
@@ -85,7 +82,7 @@ public class JiraChangesetPreProcessorTest extends JiraTestBase {
     processor.process(changeset);
     verify(changeset).setDescription("_TST-1_ are ready to review");
   }
-  
+
   /*
    * Testing {@link JiraChangesetPreProcessor#process(Changeset)}.
    */
@@ -95,7 +92,7 @@ public class JiraChangesetPreProcessorTest extends JiraTestBase {
     processor.process(changeset);
     verify(changeset).setDescription("_TST-1_, _TST-2_ and _TST-3_ are ready to review");
   }
-  
+
   /*
    * Testing {@link JiraChangesetPreProcessor#process(Changeset)} without issue key.
    */
@@ -104,5 +101,9 @@ public class JiraChangesetPreProcessorTest extends JiraTestBase {
     description("description without key");
     processor.process(changeset);
     verify(changeset).setDescription("description without key");
+  }
+
+  protected void description(String description){
+    when(changeset.getDescription()).thenReturn(description);
   }
 }
