3101ccafa9b10feff8fee9300d53a26a04e12a50
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2025-11-18 09:01:17 +0000

    Fix case for type filed in Jira REST API
    Squash commits of branch bugfix/role:
    
    - Fix case for type filed in Jira REST API
    
    - Add comment

--- src/test/java/sonia/scm/jira/JiraCommentatorTest.java
+++ src/test/java/sonia/scm/jira/JiraCommentatorTest.java
@@ -26,8 +26,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import sonia.scm.jira.config.JiraConfiguration;
 import sonia.scm.jira.rest.RestApi;
 import sonia.scm.jira.rest.RestComment;
-import sonia.scm.jira.rest.RestVisibility;
-import sonia.scm.jira.rest.RestVisibilityType;
 import sonia.scm.jira.rest.property.RestInternalProperty;
 
 import java.io.IOException;
@@ -75,7 +73,7 @@ class JiraCommentatorTest {
 
     RestComment comment = commentAndVerify("SCM-4121", "Awesome Shit");
     assertThat(comment.getVisibility()).isNotNull().satisfies(restVisibility -> {
-      assertThat(restVisibility.getType()).isEqualTo(RestVisibilityType.ROLE);
+      assertThat(restVisibility.getType()).isEqualTo("role");
       assertThat(restVisibility.getValue()).isEqualTo("vogon");
     });
   }
